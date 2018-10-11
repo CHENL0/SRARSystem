158224e8509b12375d6cdbe76d54aba2b50d6ae5
CHENL0 <893457378@qq.com>
2018-10-11 21:59:24 +0800

    first commit

---
+++ src/main/java/com/example/srarsystem/SrarsystemApplication.java
@@ -0,0 +1,12 @@
+package com.example.srarsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrarsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrarsystemApplication.class, args);
+    }
+}
